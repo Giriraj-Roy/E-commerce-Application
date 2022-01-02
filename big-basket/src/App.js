f94a87791ee2058098703652f6ff948e8bf540d7
Giriraj Roy <girirajroy2001@gmail.com>
2022-01-02 19:50:48 +0530

    Loader component

--- big-basket/src/App.js
+++ big-basket/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, useParams} from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import Categories from './Category/Categories';
 import Home from './Home';
 import Details from './Details/Details';
@@ -16,7 +16,7 @@ const App = () => {
         <Route exact path="/" element={<Home/>} />
         <Route path="/categories" element={<Categories/>} />
         <Route path= {"/products/:id"}  element= {<Details />} />
-        
+        <Route path="/games" element={<Categories/>} />
       </Routes>
       
     </React.Fragment>
--- big-basket/src/Home.js
+++ big-basket/src/Home.js
@@ -2,17 +2,20 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Product from './Product';
 import Title from './Title/Title';
+import Loading from './loader/Loading';
 // import Footer from './Footer/Footer';
 
 const Home = () => {
 
     const [ items, setItems] = useState([]);
+    const [ loading , setLoading ] = useState(true);
 
     useEffect( () => {
         const fetchItems = async ()=> {
             const result = await axios('https://fakestoreapi.com/products');
             setItems(result.data)
             console.log(result.data)
+            setLoading(false);
         }
         fetchItems()        
     },[])
@@ -21,7 +24,7 @@ const Home = () => {
         <div className='homecontainer'>
             <Title/>
             <div className="grid-container">
-                { items.map( item => (
+                { loading ? <Loading/> : items.map( item => (
                     <Product key={items.id} item={item} />
                 ))
                 }            
